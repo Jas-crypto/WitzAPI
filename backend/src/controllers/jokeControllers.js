d52948447d9dc4542e2ac00aaba4b27e80a717b2
melanieweidl <90407863+melanieweidl@users.noreply.github.com>
2022-03-28 14:51:51 +0200

    edited jokeControllers.js

--- backend/src/controllers/jokeControllers.js
+++ backend/src/controllers/jokeControllers.js
@@ -1,29 +1,29 @@
 import { check, validationResult } from "express-validator";
+import mongoose from "mongoose";
 
-const jokes = [
-  {
-    id: 0,
-    title: "Project Hail Mary",
-    text: "Andy Weir",
-    pages: 496,
-  },
-  {
-    id: 1,
-    title: "Harrow the Ninth",
-    text: "Tamsyn Muir",
-    pages: 512,
-  },
-];
+const jokeSchema = new mongoose.Schema( {
+  title: String,  
+  text: String,
+  category: String
+});
+
+const jokes= await jokes.find();
+
 export const getJokes = (req, res) => {
   res.status(200).send(jokes);
 };
 export const getJokeById = (req, res) => {
-  //fehlermeldung 404 einfügen, ob Joke existiert
   let joke = jokes.find((j) => j.id == req.params.id);
+  if (joke.isEmpty()) {
+    return res.status(404).send(`${joke.id} not found`);
+  } 
   res.status(200).send(joke);
 };
 export const getJokeByTitle = (req, res) => {
   let result = jokes.filter((joke) => joke.title == req.query.title);
+  if (joke.isEmpty()) {
+    return res.status(404).send(`${joke.title} not found`);
+  } 
   res.status(200).send(result);
 };
 
@@ -35,9 +35,29 @@ export const addJoke = (req, res) => {
   jokes.push(req.bod);
   res.status(201).send(`Added ${joke.title} to joke collection`);
 };
-
+export const editJoke=(req, res) => {
+  let joke = jokes.find((j) => j.id == req.params.id);
+  if (joke.isEmpty()) {
+    return res.status(404).send(`${joke.id} not found`);
+  } 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  jokes.update(req.bod);
+  res.status(201).send(`Edited ${joke.title} in joke collection`);
+};
+export const deleteJoke= (req, res) => {
+  let joke = jokes.find((j) => j.id == req.params.id);
+  if (joke.isEmpty()) {
+    return res.status(404).send(`${joke.id} not found`);
+  } 
+  jokes.deleteOne(req.id);
+  res.status(201).send(`Deleted ${joke.title} in joke collection`);
+};
 // attached as second param in a route
 export const newJokeValidators = [
   check("title").notEmpty().withMessage("Title field required"),
   check("text").notEmpty().withMessage("Text field required"),
+  check("category").notEmpty().withMessage("Category field required")
 ];
--- backend/src/routes/jokesRoutes.js
+++ backend/src/routes/jokesRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
+import { editJoke } from "../controllers/jokeControllers.js";
 import {
   getJokes,
   getJokeById,
   getJokeByTitle,
   addJoke,
+  editJoke,
+  deleteJoke,
   newJokeValidators,
 } from "../controllers/jokesControllers.js";
 
@@ -13,5 +16,7 @@ router.get("/", getJokes);
 router.get("/:id", getJokeById);
 router.get("/search", getJokeByTitle);
 router.post("/", newJokeValidators, addJoke);
+router.put("/:id", newJokeValidators, editJoke);
+router.delete("/:id", deleteJoke)
 
 export default router;
